78aaf96fcb1371bd3b29866f7ea0e4047c8c10b0
Masud Rana <masudrana58855@gmail.com>
2023-10-29 19:50:48 +0600

    install json server and fetching data home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,32 @@
-const HomePage = () => {
+import Products from "@/components/UI/Products";
+
+
+export const getProductData = async()=>{
+  const res = await fetch("http://localhost:5000/watches", {
+    cache: "force-cache"
+  });
+  const data = await res.json();
+
+  return data;
+}
+
+const HomePage = async() => {
+
+  // const res = await fetch("http://localhost:5000/watches", {
+  //   cache: "force-cache"
+  // });
+  // const data = await res.json();
+
+  // console.log(data);
+
+  const data = await getProductData();
+
   return (
     <div>
       <h1 className="text-4xl text-center my-10">Welcome To Watch Gallery</h1>
-      <div className="col-span-9 grid grid-cols-3 gap-5 p-10 w-[80%] mx-auto"></div>
+      <div className="col-span-9 grid grid-cols-3 gap-5 p-10 w-[80%] mx-auto">
+        {data?.map(product=> <Products key={product?.id} product={product}></Products>)}
+      </div>
     </div>
   );
 };
